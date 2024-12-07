2663563d27989feb1683a8e58773b35e94099334
N0TLUN4 <113392955+N0TLUN4@users.noreply.github.com>
2024-12-07 12:57:14 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./docs/**/*.html", "./docs/**/*.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cursive: ['"Dancing Script"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
